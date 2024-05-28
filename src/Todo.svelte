daa919a2b656c652888186ad7804a46afd06c034
shun <warnshun@gmail.com>
2024-05-28 17:58:32 +0800

    refactor: add TodoAdd component

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,31 +1,13 @@
 <script>
-  import TodoList from "./TodoList.svelte";
+  import TodoAdd from "./TodoAdd.svelte";
+import TodoList from "./TodoList.svelte";
   import TodoTotal from "./TodoTotal.svelte";
-
-  let text = "";
 
   let tasks = [
     { id: 1, text: "Task 1", isChecked: false, isComplete: false },
     { id: 2, text: "Task 2", isChecked: true, isComplete: true },
     { id: 3, text: "Task 3", isChecked: false, isComplete: false },
   ];
-
-  const addTask = () => {
-    if (text.length <= 0) {
-      alert("Please enter task");
-      return;
-    }
-
-    const newTask = {
-      id: tasks.length + 1,
-      text: text,
-      isChecked: false,
-      isComplete: false,
-    };
-
-    tasks = [...tasks, newTask];
-    text = "";
-  };
 
   const updateTasks = (e) => {
     tasks = e.detail;
@@ -39,15 +21,7 @@
 
   <TodoTotal {tasks} on:updateTasks={updateTasks} />
 
-  <div class="todo-add-area">
-    <input
-      class="todo-add-input"
-      bind:value={text}
-      type="text"
-      placeholder="Add new task"
-    />
-    <button class="todo-add-button" on:click={addTask}>Add</button>
-  </div>
+  <TodoAdd {tasks} on:updateTasks={updateTasks} />
 </div>
 
 <style lang="scss" tpye="text/scss">
@@ -66,19 +40,6 @@
       text-align: center;
       font-size: 36px;
       font-weight: 600;
-    }
-
-    .todo-add-area {
-      display: flex;
-
-      .todo-add-input {
-        flex: 0;
-        min-width: 200px;
-      }
-      .todo-add-button {
-        flex: 0;
-        min-width: 80px;
-      }
     }
   }
 </style>
---
+++ src/TodoAdd.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let tasks = [];
+  let text = "";
+
+  const dispatch = createEventDispatcher();
+
+  const addTask = () => {
+    if (text.length <= 0) {
+      alert("Please enter task");
+      return;
+    }
+
+    const newTask = {
+      id: tasks.length + 1,
+      text: text,
+      isChecked: false,
+      isComplete: false,
+    };
+
+    dispatch("updateTasks", [...tasks, newTask]);
+
+    text = "";
+  };
+</script>
+
+<div class="todo-add-area">
+  <input
+    class="todo-add-input"
+    bind:value={text}
+    type="text"
+    placeholder="Add new task"
+  />
+  <button class="todo-add-button" on:click={addTask}>Add</button>
+</div>
+
+<style lang="scss" tpye="text/scss">
+  .todo-add-area {
+    display: flex;
+
+    .todo-add-input {
+      flex: 0;
+      min-width: 200px;
+    }
+    .todo-add-button {
+      flex: 0;
+      min-width: 80px;
+    }
+  }
+</style>
